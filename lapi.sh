807cba9f76ebd82d705be12a72fb6d51ef798bbf
bokwoon <bokwoon.c@gmail.com>
2019-02-01 01:26:33 +0800

    Added the beginnings of the recursive algorithm
    called 'lapitest' and 'walk'. Very Promising.
    Mad props to
    https://starkandwayne.com/blog/bash-for-loop-over-json-array-using-jq/
    who gave the illuminating strategy of base64 encoding each json element
    into an ASCII string so that bash can properly loop over the elements,
    then in each loop iteration base64-decode the ASCII string back to json
    to use jq to parse the internal info.
    
    Wait, if I'm encoding and decoding every single loop isn't the final
    product going to be very slow? Oh no.

--- lapi.sh
+++ lapi.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
+export CG3002="417770fc-b119-488d-8c4b-bb70b88d7ac5"
+export CS2102="8f2c5b07-a5be-4d86-bb1c-8839d5b72292"
+export EE4204="fb80965b-191c-49c3-aa30-a5c890615721"
+export EG2401="f559e6e3-eb62-45b4-97e3-cbdb57710eae"
+export GES1002="1c752f6a-52f7-45f4-8a06-2d8b39878593"
+export MA1512="7d6b57ce-4fd6-4f63-b94e-08c6306f985d"
+export MA1513="08794bbd-b65a-4389-ad06-078b09fc729e"
+
 # lapi core API
 lapi_modules_taken() {
   curl "https://ivle.nus.edu.sg/api/Lapi.svc/Modules_Taken?APIKey=$LAPIkey&AuthToken=$AuthToken&StudentID=$StudentID" | jq .
 }
 
 lapi_modules_search() {
-  # $1 - ModuleCode e.g. MA1513
   curl "https://ivle.nus.edu.sg/api/Lapi.svc/Modules_Search?APIKey=$LAPIkey&AuthToken=$AuthToken&IncludeAllInfo=false&ModuleCode=$1" | jq .
 }
 
 lapi_workbins() {
-  # $1 - CourseID e.g. 08794bbd-b65a-4389-ad06-078b09fc729e
   curl "https://ivle.nus.edu.sg/api/Lapi.svc/Workbins?APIKey=$LAPIkey&AuthToken=$AuthToken&CourseID=$1&TitleOnly=false&WorkbinID=$2" | jq .
 }
 
 lapi_downloadfile() {
-  # $1 - CourseID e.g. 5444db22-b035-406a-9c46-2cdac6e30bd3
   curl "https://ivle.nus.edu.sg/api/downloadfile.ashx?APIKey=$LAPIkey&AuthToken=$AuthToken&ID=$1&target=workbin" | jq .
 }
 
@@ -73,14 +78,6 @@ modules_taken() {
   rm temp*.json
 }
 
-export CG3002="417770fc-b119-488d-8c4b-bb70b88d7ac5"
-export CS2102="8f2c5b07-a5be-4d86-bb1c-8839d5b72292"
-export EE4204="fb80965b-191c-49c3-aa30-a5c890615721"
-export EG2401="f559e6e3-eb62-45b4-97e3-cbdb57710eae"
-export GES1002="1c752f6a-52f7-45f4-8a06-2d8b39878593"
-export MA1512="7d6b57ce-4fd6-4f63-b94e-08c6306f985d"
-export MA1513="08794bbd-b65a-4389-ad06-078b09fc729e"
-
 workbins() {
   if [[ "$1" == "" ]]; then
     CourseID=$MA1513
@@ -99,4 +96,29 @@ populate_jsons() {
   workbins $GES1002 > ges1002.json
   workbins $MA1512 > ma1512.json
   workbins $MA1513 > ma1513.json
+}
+
+lapitest() {
+  # export testjson=$(< test.json)
+  export folders=$(< test.json jq -r .[].Folders)
+  for row in $(< test.json | jq -r '.[].Folders[] | @base64'); do
+    walk $(echo $row | base64 --decode)
+    echo ""
+  done
+}
+
+walk() {
+  if [[ $(echo $1 | jq -r .Title) != "null" ]]; then
+    echo "This is a Workbin"
+  elif [[ $(echo $1 | jq -r .FolderName) != "null" ]]; then
+    echo "This is a Folder called $(echo $1 | jq -r .FolderName)"
+    for row in $(echo $1 | jq -r '.Folders[] | @base64'); do
+      walk $(echo $row | base64 --decode)
+    done
+    for row in $(echo $1 | jq -r '.Files[] | @base64'); do
+      walk $(echo $row | base64 --decode)
+    done
+  elif [[ $(echo $1 | jq -r .FileName) != "null" ]]; then
+    echo "This is a File called $(echo $1 | jq -r .FileName)"
+  fi
 }
