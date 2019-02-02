99b7c549cba06a2e21078262615fc231fe1086d6
bokwoon <bokwoon.c@gmail.com>
2019-02-02 13:01:50 +0800

    Updated test.json

--- ivled.go
+++ ivled.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 type File struct {
-	ID string
+	ID       string
 	FileName string
 	FileType string
 	FileSize int
@@ -11,13 +15,21 @@ type File struct {
 
 type Folder struct {
 	FolderName string
-	Folders []Folder
-	Files []File
+	Folders    []Folder
+	Files      []File
 }
 
 type Workbin struct {
-	Title string
+	Title   string
 	Folders []Folder
+}
+
+func workbins() {
+	resp, err := http.Get(os.ExpandEnv("https://ivle.nus.edu.sg/api/Lapi.svc/Modules_Search?APIKey=$LAPIkey&AuthToken=$AuthToken&IncludeAllInfo=false&ModuleCode=$1"))
+	if err != nil {
+		// handle err
+	}
+	defer resp.Body.Close()
 }
 
 func main() {
